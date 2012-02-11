5bb9c3f92e3c3fbb38ca8b94333ec8cd648e6160
Odin Hørthe Omdal <odin.omdal@gmail.com>
2012-02-12 00:29:33 +0100

    add rsync update line to the sync script

--- mdb2csv.sh
+++ mdb2csv.sh
@@ -5,6 +5,8 @@ else
 	namn=nmu.mdb
 fi
 
+rsync -e "ssh -i $HOME/.ssh/fosse" -avzP aasen.nynorsk.no:"/srv/vinje/0\ MEDLEM/nmu.mdb" "$namn"
+
 mdb-export $namn medl > nmu-medl.csv
 mdb-export $namn lag > nmu-lag.csv
 mdb-export $namn bet > nmu-bet.csv
