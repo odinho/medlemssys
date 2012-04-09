2a240dbb7633ff7b68a354a541ccbb3792dd0d93
Odin Hørthe Omdal <odin.omdal@gmail.com>
2012-04-09 20:46:23 +0200

    Slett denne vekas stats viss me koyrer på ny

--- statistikk/views.py
+++ statistikk/views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # vim: ts=4 sts=4 expandtab ai
+from django.db import IntegrityError
 from django.http import HttpResponse
 import datetime, json
 
@@ -10,19 +11,25 @@ def update(request):
     lokallag = Lokallag.objects.all()
 
     llstat = []
+    LokallagStat.objects.filter(veke="{:%Y-%W}".format(datetime.datetime.now())).delete()
+
     for llag in lokallag:
         teljande_list = list(llag.medlem_set.teljande().values_list('pk', flat=True))
         interessante_list = list(llag.medlem_set.interessante().values_list('pk', flat=True))
-        llstat.append(LokallagStat.objects.create(
-                lokallag = llag,
-                veke = "{:%Y-%W}".format(datetime.datetime.now()),
+        try:
+            llstat.append(LokallagStat.objects.create(
+                    lokallag = llag,
+                    veke = "{:%Y-%W}".format(datetime.datetime.now()),
 
-                teljande = json.dumps(teljande_list),
-                interessante = json.dumps(interessante_list),
+                    teljande = json.dumps(teljande_list),
+                    interessante = json.dumps(interessante_list),
 
-                n_teljande = llag.medlem_set.teljande().count(),
-                n_interessante = llag.medlem_set.interessante().count(),
-                n_ikkje_utmelde = llag.medlem_set.ikkje_utmelde().count(),
-                n_totalt = llag.medlem_set.count(),
-            ))
+                    n_teljande = llag.medlem_set.teljande().count(),
+                    n_interessante = llag.medlem_set.interessante().count(),
+                    n_ikkje_utmelde = llag.medlem_set.ikkje_utmelde().count(),
+                    n_totalt = llag.medlem_set.count(),
+                ))
+        except IntegrityError:
+            print "Already have this week"
+            return HttpResponse("Har allereie denne veka", content_type="text/plain; charset=utf-8")
     return HttpResponse(unicode(llstat), content_type="text/plain; charset=utf-8")
