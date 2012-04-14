32519ae7e3fa2c8516047eba8a8bf2c43aff41c2
Odin Hørthe Omdal <odin.omdal@gmail.com>
2012-04-14 15:20:56 +0200

    statistikk importerte ikkje lokallagsmodellen korrekt

--- statistikk/models.py
+++ statistikk/models.py
@@ -3,7 +3,7 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
-from medlem.models import Lokallag
+from medlemssys.medlem.models import Lokallag
 
 class LokallagStat(models.Model):
     lokallag = models.ForeignKey(Lokallag)
