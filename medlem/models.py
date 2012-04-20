074650d8add2b39073674861c041b1c4bea2132f
Odin Hørthe Omdal <odin.omdal@gmail.com>
2012-04-20 18:34:05 +0200

    Tabelloppdatering, verva_av-felt

--- medlem/models.py
+++ medlem/models.py
@@ -181,6 +181,7 @@ class Medlem(models.Model):
             choices=INNMELDINGSTYPAR, blank=True, null=True)
     innmeldingsdetalj = models.CharField(_("detalj om innmelding"), max_length=255,
         blank=True, null=True, help_text=_("Skriv inn vervemedlem i hakeparantesar ([1234])"))
+    verva_av = models.ForeignKey('Medlem', blank=True, null=True)
 
     # Tilkopla felt
     lokallag = models.ForeignKey(Lokallag, blank=True, null=True)
