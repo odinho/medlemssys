9efb1a6c472d6db2737576a443955f66b4fb6212
Odin Hørthe Omdal <odin.omdal@gmail.com>
2013-04-03 18:15:48 +0200

    Skriv ein betre endringslogg når brukar sjølv oppdaterer val

--- medlem/views.py
+++ medlem/views.py
@@ -36,15 +36,26 @@ def edit_medlem(request, id, nykel):
         post_val += request.POST.get('val_tlf',  '')
         post_val += request.POST.get('val_nyhendebrev',  '')
         with reversion.create_revision():
-            reversion.set_comment('Brukar oppdaterte {0}'.format(form.changed_data))
+            added = []
+            removed = []
             for tittel in ('Ikkje epost', u'Ikkje Motmæle', 'Ikkje ring', 'Ikkje epost',
                            'Ikkje Norsk Tidend', 'Ikkje nyhendebrev'):
                            #'Ikkje lokallagsepost', 'Ikkje SMS', 'Ikkje nyhendebrev'):
                 val_obj = Val.objects.get(tittel=tittel)
                 if tittel in post_val:
+                    if not m.val_exists(tittel):
+                        added.append(tittel)
                     m.val.add(val_obj)
                 else:
+                    if m.val_exists(tittel):
+                        removed.append(tittel)
                     m.val.remove(val_obj)
+            comment = "Brukar oppdaterte {0}.".format(form.changed_data)
+            if added:
+                comment += " La til: {0}.".format(added)
+            if removed:
+                comment += " Tok vekk: {0}.".format(removed)
+            reversion.set_comment(comment)
             form.save()
 
     form = EndraMedlemForm(instance=m)
