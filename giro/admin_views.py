485b78893f4f4212e2fc3485e12d8e8fcf8337f1
Odin Hørthe Omdal <odinho@opera.com>
2013-05-28 23:04:08 +0200

    girosearch: Måte å søkja opp giroar

--- giro/admin_views.py
+++ giro/admin_views.py
@@ -1,7 +1,17 @@
+# -*- coding: utf-8 -*-
+# vim: ts=4 sts=4 expandtab ai
+
+from django import forms
+from django.db.models import Q
 from django.shortcuts import render
 
 from medlemssys.medlem.models import Giro
 from .views import send_ventande_rekningar
+
+class GiroSearch(forms.Form):
+    search_data = forms.CharField(widget=forms.Textarea(
+                      attrs={'class': 'vLargeTextField',
+                             'rows': 25}))
 
 def send(request):
     if request.method == 'POST':
@@ -14,5 +24,40 @@ def send(request):
     ventar = Giro.objects.filter(status='V').select_related('medlem')
     return render(request, 'admin/giro/send.html', {
         'ventar': ventar,
-        'title': 'Send ventande giroar',
+        'title': "Send ventande giroar",
+    })
+
+def manual_girosearch(request):
+    if request.method == 'POST':
+        form = GiroSearch(request.POST)
+        form.is_valid()
+        giroar = []
+        giropk_set = set()
+        for line in form.cleaned_data['search_data'].split():
+            try:
+                line = int(line.strip())
+            except:
+                giroar.append({'search': line, 'error': "Kunne ikkje gjera om til tal"})
+                continue
+            giro = Giro.objects.filter(
+                Q(kid=line) |
+                Q(pk=line) |
+                Q(medlem__pk=line))
+            if not giro:
+                giroar.append({'search': line, 'error': "Fann ikkje giro"})
+            else:
+                giropk_set.add(str(giro[0].pk))
+                giroar.append({'search': line, 'giro': giro[0]})
+        admin_url = '/admin/medlem/giro/?id__in={}'.format(','.join(giropk_set))
+        return render(request, 'admin/manual_girosearch.html', {
+            'admin_url': admin_url,
+            'form': form,
+            'giroar': giroar,
+            'title': u"Manuelt girosøk",
+        })
+    else:
+        form = GiroSearch()
+    return render(request, 'admin/manual_girosearch.html', {
+        'form': form,
+        'title': "Manuelt girosøk",
     })
