9f723a3c63ee695b1ea19cd087fde5b764acbd59
Torstein Husebø <torstein@huseboe.net>
2014-03-21 22:13:24 +0100

    Correct typo in license header

--- medlemssys/giro/views.py
+++ medlemssys/giro/views.py
@@ -5,12 +5,12 @@
 
 # This file is part of Medlemssys.
 
-# Foobar is free software: you can redistribute it and/or modify
+# Medlemssys is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 
-# Foobar is distributed in the hope that it will be useful,
+# Medlemssys is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
